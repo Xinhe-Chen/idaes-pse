0bbc9af34eccccf96cc8c011fe3ab79dd9eb71cb
Xinhe Chen <xchen24@nd.edu>
2025-07-21 15:31:54 -0400

    fix post-solving function bugs

--- idaes/apps/grid_integration/pricetaker/rolling_horizon_price_taker.py
+++ idaes/apps/grid_integration/pricetaker/rolling_horizon_price_taker.py
@@ -657,8 +657,8 @@ class StochasticPriceTaker(ConcreteModel):
         # get the power output from the model
         power_output = self._get_operation_vars(1, var_name)
         
-        # calculate the actual revenue
-        actual_revenue = sum(actual_price[t] * power_output[1][t] for t in self.set_planning_horizon)
+        # calculate the actual revenue, the actual price is indexed from 0.
+        actual_revenue = sum(actual_price[t-1] * power_output[1][t] for t in self.set_planning_horizon)
 
         return actual_revenue
     
